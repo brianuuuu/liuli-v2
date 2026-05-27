0a3eaaaf0a3f6a45a2164ba0b0668be92c652fdc
brianuuuu <brianuuuu@hotmail.com>
2026-05-27 21:26:31 +0800

    删除过时的文档

--- docs/track_discovery_legacy_table_cleanup.sql
+++
@@ -1,9 +0,0 @@
--- Manual cleanup for legacy Track Discovery tables.
--- Do not run this file unless the operator has explicitly approved dropping
--- these tables for the current database.
-
-DROP TABLE IF EXISTS track_related_stock;
-DROP TABLE IF EXISTS track_thesis;
-DROP TABLE IF EXISTS track_validation_indicator;
-DROP TABLE IF EXISTS track_evidence;
-DROP TABLE IF EXISTS track_heat_snapshot;
