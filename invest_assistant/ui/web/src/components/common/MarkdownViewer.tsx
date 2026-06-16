626695876c0e72c0ebf05ea776de2c504775485a
brian <brianuuuu@hotmail.com>
2026-06-16 16:57:59 +0800

    style: refine web content and markdown styles

--- invest_assistant/ui/web/src/components/common/MarkdownViewer.tsx
+++ invest_assistant/ui/web/src/components/common/MarkdownViewer.tsx
@@ -1,6 +1,7 @@
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import "github-markdown-css/github-markdown.css";
+import "./MarkdownViewer.css";
 
 type MarkdownViewerProps = {
   content: string;
